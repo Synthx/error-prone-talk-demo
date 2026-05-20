b597b560bf2f9845a4742783a0c0acd00d27eb22
Taniel Rémi <taniel.remi@gmail.com>
2026-05-20 07:46:20 +0200

    ✨ make user name dynamic via command line arguments
    Refactors the DemoApplication to accept the user's name from the first command-line argument, allowing for dynamic input instead of a hardcoded value. An IllegalArgumentException is now thrown if no arguments are provided.

--- src/main/java/DemoApplication.java
+++ src/main/java/DemoApplication.java
@@ -1,6 +1,10 @@
 import com.jetdev.devlille.devlilleerrorpronedemo.dto.User;
 
-void main() {
-    final String name = new User("Pinpin").name();
+void main(final String[] args) {
+    if (args.length == 0) {
+        throw new IllegalArgumentException("No arguments provided.");
+    }
+
+    final String name = new User(args[0]).name();
     System.out.printf("Hello %s%n", name);
 }
